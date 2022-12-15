99e776bf28484a6930b78538e9755def37f6f527
Polyhalov <apolyhalov@gmail.com>
2022-12-15 22:28:47 +0200

    dz3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,25 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const input = document.querySelector('.feedback-form input');
+const textarea = document.querySelector('.feedback-form textarea');
+
+
+form.addEventListener('input', throttle(textForm,500));
+function textForm(event) {
+    const formInfo = { email: input.value, message: textarea.value };
+    localStorage.setItem('feedback-form-state', JSON.stringify(formInfo));
+}
+const local = JSON.parse(localStorage.getItem('feedback-form-state'));
+if (local) {
+    input.value = local.email;
+    textarea.value = local.message;
+}
+form.addEventListener('submit', onSubmitClick)
+function onSubmitClick(event) {
+    event.preventDefault();
+    console.log({ email: input.value, message: textarea.value })
+    form.reset();
+    localStorage.removeItem('feedback-form-state')
+    
+}
