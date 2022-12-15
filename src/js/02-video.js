1c6d45afbd3dcad2072b8f6c105f4c657d1a8823
Polyhalov <apolyhalov@gmail.com>
2022-12-15 13:39:52 +0200

    dz2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,10 @@
-
+    import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+    const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+player.on('timeupdate', throttle(timeData, 1000))
+function timeData(event) {
+    localStorage.setItem('videoplayer-current-time',event.seconds)
+}    
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time'))
+   
